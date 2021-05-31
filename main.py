a42515a50f8d7d482d34a5576260b28ba21e95ed
Mark Music <markmusic999@gmail.com>
2021-05-31 04:55:03 -0600

    ft[main] => 👽 Make load_data function used to initialize x, y, and m values

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-helloWorld = "hello world"
+import numpy as np
+import matplotlib.pyplot as mpl
+import pandas as pd
+
+# The location of the model's training set (just the dataset)
+PATH = "training_set.csv"
+
+data = pd.read_csv(PATH)
+
+# Function formats and loads the data needed for the regression analysis
+
+
+def load_data():
+    data = pd.read_csv(PATH)
+    m = len(data)
+    x = np.array(data[data.columns[1]]).reshape((m, 1))
+    y = np.array(data[data.columns[2]]).reshape((m, 1))
+    return [x, y, m]
